04f0b4e463cf351e6ed2fb9f9dacbee6e48fa51f
Stan Clarke, Jr <stan@stanclarkejr.ca>
2022-04-07 23:28:26 -0400

    Removed uncessary code. Using googleapisto fetch data instead of axios

--- server/index.js
+++ server/index.js
@@ -6,15 +6,11 @@ const { YOUTUBE_API_V3 } = process.env;
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const axios = require('axios');
 const { google } = require('googleapis');
-const { nextTick } = require('process');
 const youtube = google.youtube({
   version: 'v3',
   auth: YOUTUBE_API_V3,
 });
-
-const baseAPIUrl = 'https://www.googleapis.com/youtube/v3';
 
 const app = express();
 
@@ -30,25 +26,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 //--- ENDPOINTS --- //
-app.get('/', (request, response) => {
-  response.send('Hello from our API');
-});
 
-// Using axios
+// Using googleapis instead of axios (Syntactic sugar for the url. Also returns a promise. )
 app.get('/search', async (request, response) => {
-  try {
-    const searchQuery = request.query.search_query;
-    const url = `${baseAPIUrl}/search?key=${YOUTUBE_API_V3}&type=video&part=snippet&q=${searchQuery}`;
-    const axiosResponse = await axios.get(url);
-    const titles = axiosResponse.data.items.map((item) => item.snippet.title);
-    response.send(titles);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// Using googleapis (Syntactic sugar for the url. Also returns a promise. )
-app.get('/search-with-googleapis', async (request, response) => {
   try {
     const searchQuery = request.query.search_query;
     // https://developers.google.com/youtube/v3/docs/search/list#parameters
@@ -63,7 +43,6 @@ app.get('/search-with-googleapis', async (request, response) => {
   } catch (error) {
     next(error);
   }
-});
 
 const PORT = 8000;
 
