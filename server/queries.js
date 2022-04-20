931772512935717043e959bacd77573728ca6cfa
Stan Clarke, Jr <stan@stanclarkejr.ca>
2022-04-20 12:27:41 -0400

    Experimenting with search queries

--- server/queries.js
+++ server/queries.js
@@ -1,12 +1,20 @@
 const queries = {
-  allGuitarVideos: 'guitar lessons (beginner | intermediate | advanced)',
-  allPianoVideos: 'piano lessons (beginner | intermediate | advanced)',
-  allDrumVideos: 'drum lessons (beginner | intermediate | advanced)',
-  allViolinVideos: 'violin lessons (beginner | intermediate | advanced)',
-  allSaxophoneVideos: 'saxophone lessons (beginner | intermediate | advanced)',
-  allHarmonicaVideos: 'harmonica lessons (beginner | intermediate | advanced)',
-  allBassVideos: 'bass lessons (beginner | intermediate | advanced)',
-  allMidiVideos: 'midi lessons (beginnner | intermediate | advanced)',
+  allGuitarVideos:
+    'guitar lessons (beginner | intermediate | advanced | jazz | blues)',
+  allPianoVideos:
+    'piano lessons (beginner | intermediate | advanced | jazz | blues)',
+  allDrumVideos:
+    'drum lessons (beginner | intermediate | advanced jazz | blues)',
+  allViolinVideos:
+    'violin lessons (beginner | intermediate | advanced jazz | blues)',
+  allSaxophoneVideos:
+    'saxophone lessons (beginner | intermediate | advanced jazz | blues)',
+  allHarmonicaVideos:
+    'harmonica lessons (beginner | intermediate | advanced jazz | blues)',
+  allBassVideos:
+    'bass lessons (beginner | intermediate | advanced jazz | blues)',
+  allMidiVideos:
+    'midi lessons (beginnner | intermediate | advanced jazz | blues)',
 };
 
 module.exports = queries;
